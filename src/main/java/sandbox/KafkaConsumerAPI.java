21fc7a37c17d5cd79c2b6b71e6456b167e05f877
Ershov Dariy <ershovdar@yandex.ru>
2025-01-15 16:37:46 +0300

    consumer work

--- src/main/java/sandbox/KafkaConsumerAPI.java
+++ src/main/java/sandbox/KafkaConsumerAPI.java
@@ -1,4 +1,64 @@
 package sandbox;
 
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
+import java.util.stream.StreamSupport;
+
+
+
 public class KafkaConsumerAPI {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerAPI.class);
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092, localhost:49092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+       properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaConsumerAPI");
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties)) {
+
+//            consumer.assign(List.of(
+//                    new TopicPartition("sandbox", 0),
+//                    new TopicPartition("sandbox", 1),
+//                    new TopicPartition("sandbox", 2)
+//            ));
+            consumer.subscribe(Pattern.compile("sandbox"), new MyConsumerRebalanceListener());
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5));
+
+            StreamSupport.stream(records.spliterator(), false)
+                    .forEach(record -> LOGGER.info("Record: {}", record));
+
+        }
+    }
+}
+
+class MyConsumerRebalanceListener implements ConsumerRebalanceListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerAPI.class);
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        LOGGER.info("onPartitionsRevoked: {}", partitions);
+    }
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        LOGGER.info("onPartitionsAssigned: {}", partitions);
+    }
 }
--- src/main/java/sandbox/KafkaProducerAPI.java
+++ src/main/java/sandbox/KafkaProducerAPI.java
@@ -23,61 +23,20 @@ public class KafkaProducerAPI {
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092, localhost:49092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //properties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my-transactional-id");
+        properties.put(ProducerConfig.ACKS_CONFIG, "1");
 
         try (KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties)){
-            metadata = producer.send(
-                   new ProducerRecord<String, String>("sandbox", "Hello World gnomiki")).get();
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
+//            producer.initTransactions();
+//
+//            producer.beginTransaction();
+            producer.send(
+                   new ProducerRecord<>("sandbox", "Hello World gnomiki"),
+                    (metadata, exception)-> LOGGER.info("Metadata: {}", metadata)).get();
 
-            metadata = producer.send(
-                    new ProducerRecord<String, String>("sandbox", "My-key","Hello World gnomiki, i have key")).get();
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
+            //throw new IllegalArgumentException();
+            //producer.commitTransaction();
 
-            metadata = producer.send(
-                    new ProducerRecord<String, String>("sandbox", 0,"My-key",
-                            "Hello World gnomiki, i have key and partion")).get();
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
-
-            metadata = producer.send(
-                    new ProducerRecord<String, String>("sandbox", 0,"My-key",
-                            "Hello World gnomiki, i have key, parition",
-                            List.of(new RecordHeader("Foo", "Bar".getBytes())))).get();
-
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
-
-            metadata = producer.send(
-                    new ProducerRecord<String, String>("sandbox", 1, System.currentTimeMillis(),
-                            "My-key-with-timestamp",
-                            "Hello World gnomiki, i have key and partion")).get();
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
-
-            metadata = producer.send(
-                    new ProducerRecord<String, String>("sandbox", 1, System.currentTimeMillis(),
-                            "My-key-with-timestamp",
-                            "Hello World gnomiki, i have key, parition",
-                            List.of(new RecordHeader("Foo", "Bar".getBytes())))).get();
-
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
-
-            metadata = producer.send(
-                    new ProducerRecord<>("sandbox", "Hello World gnomiki"),
-                    (md, exception)->{LOGGER.info("Callback: metadata: {}, exception == null{}", md, exception==null);
-                    }).get();
-            LOGGER.info("=================================");
-            LOGGER.info("MetaData: {}", metadata);
-            LOGGER.info("=================================");
         }
     }
 
